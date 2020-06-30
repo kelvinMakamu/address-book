0c6ed8d2706b619c64e623474483c729f01ca23c
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-06-30 14:46:55 +0300

    Modifies scripts.js file, refactors code and decomposes functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,9 @@
 	COMMONS
 
 ***************/
-const iconsDescriptionWrapper="delani-what-we-do";
 const notificationPanel = "contact-notification-panel";
 
-const portfolios= ['work1','work2','work3','work4','work5','work6','work7','work8'];
 const alphaNumericPattern = /^[a-z0-9]+$/i;
-const emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 let validateInput = (input) => {
 	return alphaNumericPattern.test(input);
@@ -16,34 +13,45 @@ let validateInput = (input) => {
 
 let clearContactFormErrors = (inputType) =>{
 	 switch(inputType){
-
 	 	case 'first-name':
-		document.getElementById("contact-first-name").innerHTML="";
+	 	$('#contact-first-name').html("");
 	 	break;
 
 	 	case 'last-name':
-		document.getElementById("contact-last-name").innerHTML="";
+	 	$('#contact-last-name').html("");
 	 	break;
 
 	 	case 'all':
-		document.getElementById("contact-first-name").innerHTML="";
-		document.getElementById("contact-last-name").innerHTML="";
+	 	clearNotificationPanel();
+	 	$('#contact-first-name').html("");
+	 	$('#contact-last-name').html("");
 	 	break;
 	 }
 }
 
 let displayNotification = (notificationPanel,alert,message) => {
 	let notification ="<div class='alert "+alert+"' role='alert'>"+message+"</div>";
-	document.getElementById(notificationPanel).innerHTML+=notification;
+	$('#'+notificationPanel).html(notification);
 }
 
 let clearNotificationPanel = () => {
-	document.getElementById(notificationPanel).innerHTML="";
+	$('#'+notificationPanel).html("");
 }
 
 let clearContactForm = () => {
 	$("input#new-first-name").val("");
   $("input#new-last-name").val("");
+}
+
+let validateContactForm = (firstName,lastName) =>{
+	if(!validateInput(firstName)){
+		isValidForm=1001;
+	}else if(!validateInput(lastName)){
+		isValidForm=1002;
+	}else{
+		isValidForm=1000;
+	}
+	return isValidForm;
 }
 
 function Contact(first, last){
@@ -63,22 +71,16 @@ Contact.prototype.fullName = function() {
 $(document).ready( () => {
 
 	$("form#new-contact").submit(function(event){
-		clearNotificationPanel();
     event.preventDefault();
 		clearContactFormErrors('all');
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    if(!validateInput(inputtedFirstName)){
-    	$("#contact-first-name").text("Please provide your first name");
-    	displayNotification(notificationPanel,'alert-danger','Please provide your first name');
-    }else if(!validateInput(inputtedLastName)){
-    	$("#contact-last-name").text("Please provide your last name");
-    	displayNotification(notificationPanel,'alert-danger','Please provide your last name');
-    }else{
+    switch(validateContactForm(inputtedFirstName,inputtedLastName)){
+    	case 1000:
 	    var addContact = new Contact(inputtedFirstName, inputtedLastName);
+	    alertType='alert-success';
+    	message=addContact.fullName()+' added to your contact list';
 	    $("ul#contacts").append("<li><span class='contact'>"+addContact.fullName()+"</span></li>");
-	    content=addContact.fullName()+' added to your contact list';
-    	displayNotification(notificationPanel,'alert-success',content);
 		  $(".contact").last().click(function() {
 			  $("#show-contact").show();
 			  $("#show-contact h4").text(addContact.firstName);
@@ -86,7 +88,21 @@ $(document).ready( () => {
 			  $(".last-name").text(addContact.lastName);
 			}); 
 	    clearContactForm();
+    	break;
+
+    	case 1001:
+    	alertType='alert-danger';
+    	message='Please provide your first name';
+    	$("#contact-first-name").text(message);
+    	break;
+
+    	case 1002:
+    	alertType='alert-danger';
+    	message='Please provide your last name';
+    	$("#contact-last-name").text(message);
+    	break;
     }
+    displayNotification(notificationPanel,alertType,message);
   }); 
 
 });
